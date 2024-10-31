d25a5a5c5cecf4a72de92c8f3878f4acbc67932d
Mohamed Rafeek <mohamed.rafeek@mohamed.rafeek-MacBook-Pro>
2024-10-31 15:11:20 -0400

    initial commit

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,37 @@
+
 class Scooter{
   // scooter code here
+  static nextSerial = 1
+  constructor(station){
+    this.station = station
+    this.user=null
+    this.serial=Scooter.nextSerial
+    Scooter.nextSerial++
+    this.charge=100
+    this.isBroken=false
+
+  }
+
+  rent(user){
+    if(this.charge < 20){
+      throw new Error('scooter needs to charge')
+    }
+    if(this.charge > 20 && this.isBroken == true){
+      throw new Error('scooter needs repair')
+    }
+    if(this.isBroken == true){
+      throw new Error('scooter needs repair')
+    }
+
+    this.station = null
+    this.user = user
+
+  }
+  dock(station){
+    this.station = station
+    this.user = null
+  }
+
 }
 
 
