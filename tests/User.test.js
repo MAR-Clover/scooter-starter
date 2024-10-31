5b2866ca628f7ef6f38245768b8e7e48e0722f01
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-10-31 16:31:31 -0400

    Add Scooter and User classes with their tests

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,19 +1,26 @@
 const User = require('../src/User');
-
-const user = new User("Joe Bloggs", "test123", 21);
-
-// User tests here
-describe("User property tests", () => {
-  // test username
-  test("username should be a string", () => {
-    expect(typeof user.username).toBe("string");
-  })
-  // test password
-
-  // test age
-})
+const { describe, expect, it } = require("@jest/globals");
 
 
-// test login
+describe("user.login(password)", () => {
+  const user = new User('mel_yef', '123', 24);
 
-// test logout
+  it("logs a user in if the password is correct", () => {
+    user.login('123'); // Call the login method
+    expect(user.LoggedIn).toBe(true); // Check if LoggedIn is true
+  });
+
+  it("throws an error if the password is incorrect", () => {
+    expect(() => user.login('222')).toThrow("Incorrect Password");
+  });
+});
+
+describe("user.logout()", () => {
+  const user = new User('mel_yef', '123', 24);
+
+  it("logs a user out", () => {
+    user.login('123'); // Log in first
+    user.logout(); // Log out
+    expect(user.LoggedIn).toBe(false); // Check if LoggedIn is false
+  });
+});
